314b0d207bc53745d525364f8f13185e4d817af0
mihkelal <mihkel.alavere@eesti.ee>
2021-01-28 22:26:13 +0200

    Fix bug in auth

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -12,7 +12,7 @@ defmodule RumblWeb.Auth do
   def login(conn, user) do
     conn
     |> assign(:current_user, user)
-    |> put_session(:current_user, user)
+    |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
 end
