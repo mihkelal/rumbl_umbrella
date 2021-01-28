c839fdb72f9389d193343918db44ba540a44327d
mihkelal <mihkel.alavere@eesti.ee>
2021-01-28 22:05:26 +0200

    Implement logging in

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -40,4 +40,20 @@ defmodule Rumbl.Accounts do
     |> User.registration_changeset(attrs)
     |> Repo.insert()
   end
+
+  def authenticate_by_username_and_pass(username, password) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(password, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
+  end
 end
---
+++ lib/rumbl_web/controllers/session_controller.ex
@@ -0,0 +1,23 @@
+defmodule RumblWeb.SessionController do
+  use RumblWeb, :controller
+  alias Rumbl.Accounts
+
+  def new(conn, _params) do
+    render(conn, "new.html")
+  end
+
+  def create(conn, %{"session" => %{"username" => username, "password" => pass}}) do
+    case Accounts.authenticate_by_username_and_pass(username, pass) do
+      {:ok, user} ->
+        conn
+        |> RumblWeb.Auth.login(user)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Invalid username/password combination")
+        |> render("new.html")
+    end
+  end
+end
