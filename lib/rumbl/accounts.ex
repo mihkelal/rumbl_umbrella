a1ac857570e907e76ca0e5858ee93ff88e94437a
mihkelal <mihkel.alavere@eesti.ee>
2021-02-23 10:37:44 +0200

    Add Decorating Entries with Application Data

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,6 +2,9 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context.
   """
+
+  import Ecto.Query
+
   alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
@@ -19,6 +22,10 @@ defmodule Rumbl.Accounts do
 
   def list_users do
     Repo.all(User)
+  end
+
+  def list_users_with_ids(ids) do
+    Repo.all(from(u in User, where: u.id in ^ids))
   end
 
   def change_user(%User{} = user) do
